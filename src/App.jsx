9a84f0e67f39a93e1d4ce68140fd478276ffcdb5
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-06 15:35:17 +0530

    usecontect

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+ 
+
+  return (
+    <UserContextProvider>
+
+
+   <div className='flex justify-center align-center'>
+   <Login/>
+    <Profile/>
+   </div>
+    </UserContextProvider>
+ 
+  
+  )
+}
+
+export default App
